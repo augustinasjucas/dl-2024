d654e158045021a4ff0f3783ab4fa76c6debaa1c
Jan Kulik <jan.z.kulik@gmail.com>
2025-01-04 00:03:19 +0100

    Added device recognition

--- reproduce.py
+++ reproduce.py
@@ -6,7 +6,9 @@ from common.models import CIFAR_CNN_1
 from common.train import CLTraining
 import wandb
 
+
 def main():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Here, define the common wandb parameters that will be called on wandb.init() in the CLTraining and
     # possibly other metrics.
@@ -19,7 +21,7 @@ def main():
     wandb.Settings(quiet=True)
 
     # Get the raw dataset
-    full_train_dataset_cifar100, full_test_dataset_cifar100 = get_cifar('100')
+    full_train_dataset_cifar100, full_test_dataset_cifar100 = get_cifar("100")
 
     # Get the tasks dataset. Look at how these are implemented and just add new classes if you need to.
     task5 = FiveImageTasksCifar(full_train_dataset_cifar100, full_test_dataset_cifar100)
@@ -27,32 +29,28 @@ def main():
     # Get the model. IMPORTANT: move it to the needed device HERE.
     # Do NOT edit the training loops to move the model to the device there, because
     # that would mess up FlorianProbing metric (as it needs references to layers).
-    model = CIFAR_CNN_1().to("cuda")
+    model = CIFAR_CNN_1().to(device)
 
     # Define the Florian probing metric
     florian_probing_metric = FlorianProbing(
         # The order of layers to freeze. On the ith iteration, the first i layers are frozen.
-        layers_order = [
+        layers_order=[
             ([model.conv1], "conv1"),
             ([model.conv2], "conv2"),
             ([model.conv3], "conv3"),
-            ([model.fc1], "fc1")
+            ([model.fc1], "fc1"),
         ],
-
         # The list of all layers with parameters. Needed for reinitializtion of layers. Hurts to specify this, but I don't know a better way.
-        all_layers = [model.conv1, model.conv2, model.conv3, model.fc1, model.fc2],
-
+        all_layers=[model.conv1, model.conv2, model.conv3, model.fc1, model.fc2],
         # Define the parameters that will be needed when training on the FULL dataset (which comprises of all tasks in on dataset)
         optimizer=(torch.optim.Adam, {"lr": 0.001}),
         criterion=torch.nn.CrossEntropyLoss(),
         epochs=2,
         batch_size=256,
-
-        device="cuda",
+        device=device,
         wandb_params=wandb_params,
-
         # Will give nice visuals in W&B. Use True if you want to run sweeps, then it run all probing as a single run.
-        sweepy_logging=False
+        sweepy_logging=False,
     )
 
     # Define the metric that will take care of monitoring accuracy as loss during sequential training
@@ -66,29 +64,26 @@ def main():
     cl_training = CLTraining(
         # Give the model. IMPORTANT: has to be the same object as in FlorianProbing metric (not a copy!)
         model=model,
-
         # Define the parameters for training every task (same criterion and same # of epochs for all tasks)
         criterion=torch.nn.CrossEntropyLoss(),
         epochs=3,
-        device="cuda",
-
+        device=device,
         # Define the optimizer. It is done like this, so that I can reinitialize the optimizer for every task.
         # NOTE: this might not be optimal -> >>!
         optimizer=(torch.optim.Adam, {"lr": 0.001, "weight_decay": 0.001}),
-
         # Define the tasks to be trained on. This is a List[Pair[DataLoader, DataLoader]], with train and test loaders
         # for every task. Every "*TasksCifar" object will implement this get_tasks_zipped method.
         tasks=task5.get_tasks_zipped(batch_size=256)[:3],
-
         # Define the metrics to be used. For now, only FlorianProbing is implemented.
         metrics=[florian_probing_metric, beautiful_logging],
-        wandb_params=wandb_params
+        wandb_params=wandb_params,
     )
 
     # Run the training
     results = cl_training.run()
 
     print("Metric results:", results[1])
+
 
 if __name__ == "__main__":
 
